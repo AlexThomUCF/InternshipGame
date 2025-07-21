6778c2fb90dad5ea0d87e8f94766717bf5d2857c
Alex <al849783@ucf.edu>
2025-07-21 14:53:09 -0400

    Text and Lose state

---
+++ Assets/Scripts/LoseScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScript : MonoBehaviour
+{
+    public TaskList taskList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taskList = FindObjectOfType<TaskList>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameLost();
+    }
+
+    void GameLost()
+    {
+        if(taskList.imposterTaskArray.Length <= 0)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
--- Assets/Scripts/TextUpdate.cs
+++ Assets/Scripts/TextUpdate.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class TextUpdate : MonoBehaviour
 {
+    public TaskList taskList;
+    public TextMeshProUGUI textDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        taskList = FindObjectOfType<TaskList>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateText();
     }
+    void UpdateText()
+    {
+        string output = "";
+        foreach (GameObject obj in taskList.imposterTaskArray)
+        {
+            if(obj != null)
+            output += obj.name + "\n";
+        }
+
+        textDisplay.text = output;
+    }
+
 }
