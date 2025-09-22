7a37c87a2477b2121035f790ba746636054ea727
AlexThomUCF <al849783@ucf.edu>
2025-09-22 12:06:24 -0400

    push

--- Assets/Scripts/Player Scripts/PlayerAnimation.cs
+++ Assets/Scripts/Player Scripts/PlayerAnimation.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    public void PlayerMovementAnim()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
