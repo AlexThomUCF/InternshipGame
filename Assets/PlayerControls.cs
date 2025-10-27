8133b35021ffdf418565b74f2844df01a3099955
AlexThomUCF <al849783@ucf.edu>
2025-10-27 16:21:29 -0400

    Revert "Merge branch 'Alex-Branch' into Aim"
    This reverts commit 898048eeb252b1a278b904a6ee1368c171e375d9, reversing
    changes made to e8ce2a32aef6674059c78b996a0668ac68ca8ac8.

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -71,6 +71,15 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Button"",
+                    ""id"": ""41ac2f78-263e-425e-b61a-51b16df71187"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -216,6 +225,17 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""action"": ""Interact"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""222a3223-bac6-4e99-bf7d-6084330825be"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -229,6 +249,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
         m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
+        m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
     }
 
     ~@PlayerControls()
@@ -300,6 +321,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_Fire;
     private readonly InputAction m_Player_Interact;
+    private readonly InputAction m_Player_Aim;
     public struct PlayerActions
     {
         private @PlayerControls m_Wrapper;
@@ -309,6 +331,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
         public InputAction @Interact => m_Wrapper.m_Player_Interact;
+        public InputAction @Aim => m_Wrapper.m_Player_Aim;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -333,6 +356,9 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Interact.started += instance.OnInteract;
             @Interact.performed += instance.OnInteract;
             @Interact.canceled += instance.OnInteract;
+            @Aim.started += instance.OnAim;
+            @Aim.performed += instance.OnAim;
+            @Aim.canceled += instance.OnAim;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -352,6 +378,9 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Interact.started -= instance.OnInteract;
             @Interact.performed -= instance.OnInteract;
             @Interact.canceled -= instance.OnInteract;
+            @Aim.started -= instance.OnAim;
+            @Aim.performed -= instance.OnAim;
+            @Aim.canceled -= instance.OnAim;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -376,5 +405,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         void OnJump(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
         void OnInteract(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/NPC Scripts/CatchableCharacter.cs
+++ Assets/Scripts/NPC Scripts/CatchableCharacter.cs
@@ -10,12 +10,9 @@ public class CatchableCharacter : MonoBehaviour
     public bool isCaught { get; private set; }
     public List<Collider> ragDollParts = new List<Collider>();
 
-    private List<Rigidbody> ragdollRigidbodies = new List<Rigidbody>();
-
     private void Awake()
     {
         SetRagdollParts();
-        DisableRagdoll();
     }
     public void Catch(bool wasImposter)
     {
@@ -24,63 +21,23 @@ public class CatchableCharacter : MonoBehaviour
 
         if (agent) agent.isStopped = true;
         if (animator) animator.SetTrigger("Caught"); // Create a "Caught" trigger
-        
-        TurnOnRagDoll();
 
         // Notify round system
         RoundManager.Instance.OnCharacterCaught(gameObject, wasImposter);
     }
 
-    public void TurnOnRagDoll()
-    {
-        // Disable animator and root collider
-        if (GetComponent<BoxCollider>())
-            GetComponent<BoxCollider>().enabled = false;
-
-        animator.enabled = false;
-
-        // Enable physics for all ragdoll parts
-        foreach (Collider c in ragDollParts)
-        {
-            c.isTrigger = false;
-        }
-
-        foreach (Rigidbody rb in ragdollRigidbodies)
-        {
-            rb.isKinematic = false;
-            rb.velocity = Vector3.zero;
-        }
-    }
-
-    private void DisableRagdoll()
-    {
-        // Disable physics while animating
-        foreach (Collider c in ragDollParts)
-        {
-            c.isTrigger = true;
-        }
-
-        foreach (Rigidbody rb in ragdollRigidbodies)
-        {
-            rb.isKinematic = true;
-        }
-    }
     private void SetRagdollParts()
     {
-        ragDollParts.Clear();
-        ragdollRigidbodies.Clear();
+        Collider[] colliders = this.gameObject.GetComponentsInChildren<Collider>();
 
-        Collider[] colliders = GetComponentsInChildren<Collider>();
         foreach (Collider collider in colliders)
         {
             if (collider.gameObject != this.gameObject)
             {
+                collider.isTrigger = true;
                 ragDollParts.Add(collider);
-
-                Rigidbody rb = collider.attachedRigidbody;
-                if (rb != null)
-                    ragdollRigidbodies.Add(rb);
             }
+
         }
     }
 
